6f56de2a33bf5d8646bd32e4fd2421c45dcbc615
Fayadh <fayadh.almosawi@gmail.com>
2020-08-19 12:50:07 -0400

    add webpack client config

---
+++ webpack.config.client.js
@@ -0,0 +1,36 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./client/src/index.html",
+});
+
+module.exports = {
+  entry: "./client/src/index.js",
+  module: {
+    rules: [
+      { test: /\.ts?$/, loader: "ts-loader" },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+            plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+  output: {
+    publicPath: "./",
+    path: path.resolve(__dirname, "dist/client"),
+    filename: "index.js",
+    libraryTarget: "commonjs2",
+  },
+};

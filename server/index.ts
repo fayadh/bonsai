651529e23937c09d124005cebd7cd130c6efdd7c
Fayadh <fayadh.almosawi@gmail.com>
2020-08-19 14:46:58 -0400

    add dotenv

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,20 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import * as express from "express";
 import { ApolloServer } from "apollo-server-express";
 
 import typeDefs from "./modules/merchant/graphqlSchema";
 import resolvers from "./modules/merchant/resolvers";
 
+const { PORT = 3000 } = process.env;
+
 const app = express();
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.applyMiddleware({ app });
-app.listen({ port: 3000 }, () => {
-  console.log(`Server running on http://localhost:3000${server.graphqlPath}`);
+app.listen({ port: PORT }, () => {
+  console.log(
+    `Server running on http://localhost:${PORT}${server.graphqlPath}`
+  );
 });

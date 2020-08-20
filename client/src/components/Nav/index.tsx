8d20aadf7bb435c15658f22c9e89ab629ca95425
Fayadh <fayadh.almosawi@gmail.com>
2020-08-20 11:12:53 -0400

    connect nav component, add login action

--- client/src/components/Nav/index.tsx
+++ client/src/components/Nav/index.tsx
@@ -2,29 +2,43 @@ import * as React from "react";
 import { Nav, NavItem } from "reactstrap";
 import { GoogleLogin } from "react-google-login";
 
+import { connect } from "../../utils";
+import { login } from "../../store/slices/canonical/auth";
+
 import "./styles.css";
 
 const {
   GOOGLE_CLIENT_ID = "959206730822-j30lnpejcaab8ckvdhnakpr09fu1e2gq.apps.googleusercontent.com",
 } = process.env;
 
-console.log({ GOOGLE_CLIENT_ID });
+const actionCreators = {
+  login,
+};
 
-class Navigation extends React.Component {
+@connect(null, actionCreators)
+class Navigation extends React.Component<Navigation.Props, null> {
   render() {
+    const { login } = this.props;
+
     return (
       <Nav>
         <NavItem>
           <GoogleLogin
             clientId={GOOGLE_CLIENT_ID}
             buttonText="Login"
-            onSuccess={(res) => console.log(res)}
+            onSuccess={(res: any) => login({ token: res.accessToken })}
             onFailure={(err) => console.log(err)}
             cookiePolicy={"single_host_origin"}
           />
         </NavItem>
       </Nav>
     );
+  }
+}
+
+export namespace Navigation {
+  export interface Props {
+    login?: typeof login;
   }
 }
 

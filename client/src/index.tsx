6762f3af8d54c199ba79bdbaaeeb311b540565db
Fayadh <fayadh.almosawi@gmail.com>
2020-08-19 20:36:35 -0400

    connect router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,22 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import { createBrowserHistory } from "history";
+import createStore from "./store";
+
+const history = createBrowserHistory();
+const store = createStore(history);
 
 import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById("root")
+);

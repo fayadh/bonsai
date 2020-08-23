67a1e7f89fb4831417d1fa1f3028f82b5d5ff947
Fayadh <fayadh.almosawi@gmail.com>
2020-08-23 16:52:56 -0400

    Create admin user script, and user model changes

--- server/models/User/index.ts
+++ server/models/User/index.ts
@@ -1,15 +1,25 @@
 import { model, Model, Document, Schema } from "mongoose";
 
+import auth, { Auth } from "./auth";
 import profile, { Profile } from "./profile";
+
+export enum UserRole {
+  ADMIN = "admin",
+  USER = "user",
+}
 
 export interface User extends Document {
   email: string;
+  auth: Auth;
   profile: Profile;
+  role: UserRole;
 }
 
 export const UserSchema: Schema = new Schema({
   email: { type: String, unique: true, required: true },
+  auth: { type: auth, select: false },
   profile: { type: profile },
+  role: { type: String, enum: Object.values(UserRole), default: UserRole.USER },
 });
 
 const User: Model<User> = model("User", UserSchema);
--- server/models/User/profile.ts
+++ server/models/User/profile.ts
@@ -2,13 +2,13 @@ import { Schema } from "mongoose";
 
 export interface Profile {
   name: string;
-  givenName: string;
-  familyName: string;
+  givenName?: string;
+  familyName?: string;
 }
 
 const Profile: Schema = new Schema(
   {
-    name: { type: String, default: null },
+    name: { type: String, default: null, required: true },
     givenName: { type: String, default: null },
     familyName: { type: String, default: null },
   },

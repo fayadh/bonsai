8feabfb0ee1ef16235be801a3360c66728b8f38d
Fayadh <fayadh.almosawi@gmail.com>
2020-08-24 13:15:29 -0400

    change middleware to split auth strategies

--- server/middleware.ts
+++ server/middleware.ts
@@ -1,7 +1,8 @@
 import { Handler } from "express";
 import { OAuth2Client } from "google-auth-library";
+import * as bcrypt from "bcrypt";
 
-import User from "./models/User";
+import User, { User as IUser } from "./models/User";
 
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
 const client = new OAuth2Client(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET);
@@ -37,19 +38,74 @@ const findOrCreateUser = async ({
   return user;
 };
 
-export const authMiddleware: Handler = async (
+const loginJWT = async ({ req, res, next, token }: any) => {
+  try {
+    const userGoogleInfomation = await verify(token);
+    let user = await findOrCreateUser(userGoogleInfomation);
+    req.user = user;
+    next();
+  } catch (e) {
+    res.status(401).send();
+  }
+};
+
+const loginAdmin = async ({
+  req,
+  res,
+  next,
+  email,
+  password,
+}: any): Promise<IUser | null> => {
+  try {
+    const userWithAuth = await User.findOne({ email }).select({ auth: 1 });
+
+    const invalidCredentialsMessage = "Incorrect email or password.";
+    if (!userWithAuth) {
+      throw new Error(invalidCredentialsMessage);
+    }
+
+    if (!userWithAuth.auth) {
+      return null;
+    }
+
+    const validPassword = await bcrypt.compare(
+      password,
+      userWithAuth.auth.password.bcrypt
+    );
+
+    if (!validPassword) {
+      throw new Error(invalidCredentialsMessage);
+    }
+
+    const user = await User.findOne({ email });
+
+    req.user = user;
+    next();
+  } catch (e) {
+    res.status(401).send();
+  }
+};
+
+export const authenticationMiddleware: Handler = async (
   req: any,
   res: any,
   next: any
 ) => {
-  try {
-    const token = req.cookies["jwt"] || "";
-    const userGoogleInfomation = await verify(token);
-    let user = await findOrCreateUser(userGoogleInfomation);
-    //@ts-ignore
-    req.user = user;
-    next();
-  } catch (e) {
-    res.send(401);
+  // if trying to login with email, password combo, use password lookup strategy
+  const { operationName, variables } = req.body;
+  if (operationName == "LoginAdmin") {
+    const { email, password } = variables;
+    loginAdmin({ req, res, next, email, password });
+    return;
   }
+
+  // if token present, login with jwt strategy
+  const token = req.cookies["jwt"] || "";
+  if (token) {
+    loginJWT({ req, res, next, token });
+    return;
+  }
+
+  // otherwise user is empty
+  next();
 };

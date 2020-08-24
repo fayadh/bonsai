ecac1f16de5aed41df21e63b180347c7c64de435
Fayadh <fayadh.almosawi@gmail.com>
2020-08-24 17:52:29 -0400

    get user profile back with get user call

--- server/modules/user/graphqlSchema.ts
+++ server/modules/user/graphqlSchema.ts
@@ -1,13 +1,23 @@
 import { gql } from "apollo-server-express";
 
 const typeDefs = gql`
+  type UserProfile {
+    name: String!
+    givenName: String
+    familyName: String
+  }
+
   type User {
     email: String
+    profile: UserProfile
+    role: String
   }
+
   extend type Query {
     user: User
     users: [User!]!
   }
+
   extend type Mutation {
     loginAdmin(email: String!, password: String!): User
   }
